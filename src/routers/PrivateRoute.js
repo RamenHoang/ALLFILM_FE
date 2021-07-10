957ea38db935d4973c4485f998b829b76bfc09ff
Anh NGuyen <43921803+RamenHoang@users.noreply.github.com>
2021-07-10 21:48:14 +0700

    update ux of member screen and also update ux after clicking on film tab

--- src/routers/PrivateRoute.js
+++ src/routers/PrivateRoute.js
@@ -1,22 +1,38 @@
-import { BrowserRouter as Route, Redirect } from 'react-router-dom'
-
+import { Route, Redirect } from 'react-router-dom'
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { notification } from 'antd';
+
+const openNotification = () => {
+  const args = {
+    message: 'Cảnh báo!!!',
+    description:
+      'Bạn không được phép truy cập vào đường dẫn này. Vui lòng đăng nhập.',
+    duration: 2,
+  };
+  
+  notification.warning(args);
+};
 
 const PrivateRoute = ({ component: Component, ...rest })=>{
 
     const token = useSelector(state => state.token.token);
 
     useEffect(() => {
-        if(token.access_token === undefined) alert("Please login before book ticket")
+        if(token.access_token === undefined) {
+            openNotification();
+        }
     }, []);
   
     return(
-    <Route {...rest} render={(props) => (
-        token.access_token !== undefined
-            ? <Component {...props} />
-            : <Redirect to='/' />
-        )} />
+        <Route 
+            {...rest} 
+            render={(props) => (
+                token.access_token !== undefined
+                ? <Component {...props} />
+                : <Redirect to='/' />
+            )} 
+        />
     )
 } 
 
